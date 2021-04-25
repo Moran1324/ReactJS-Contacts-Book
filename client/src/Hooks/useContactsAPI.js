bb19230585df2e496f38c7655b4c5e6e8c954079
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2021-04-25 15:12:57 +0300

    initialize app structure

---
+++ client/src/Hooks/useContactsAPI.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function useContactsAPI() {
+  return (
+    <div>
+      Contacts Hook
+    </div>
+  )
+}
+
+export default useContactsAPI
---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+import PageContent from '../Components/PageContent'
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <NavBar />
+      <PageContent />
+    </div>
+  )
+}
+
+export default HomePage
