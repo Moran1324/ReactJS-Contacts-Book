8d13f967b2c2b165b68c07a9483347965e06d36f
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2021-04-25 17:56:01 +0300

    useContactsApi custom hook working with pagination

--- client/src/Components/PageContent.js
+++ client/src/Components/PageContent.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import useContactsAPI from '../Hooks/useContactsAPI';
 
 const PageContent = () => {
-  const data = 'data';
+  const { contacts, loadMoreContacts } = useContactsAPI();
+  console.log('contacts', contacts);
   return (
-    <div>
-      Page Content
-    </div>
+    <>
+      <div>
+        Page Content
+      </div>
+      <Button onClick={loadMoreContacts} variant="contained">
+        Load More...
+      </Button>
+    </>
   );
 };
 
--- client/src/Hooks/useContactsAPI.js
+++ client/src/Hooks/useContactsAPI.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const contactsUrl = 'https://randomuser.me/api/?results=9';
+const contactsUrlFiltered = 'https://randomuser.me/api/?inc=gender,name,nat';
 
 function useContactsAPI() {
-  return (
-    <div>
-      Contacts Hook
-    </div>
-  );
+  const [contacts, setContacts] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const contactsUrlPagination = `https://randomuser.me/api/?page=${pageNumber}&results=9&seed=abc&inc=gender,name,phone,cell,picture,email`;
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const { data } = await axios.get(contactsUrlPagination);
+      setContacts((prevState) => [...prevState, ...data.results]);
+    };
+    getContacts();
+  }, [pageNumber]);
+
+  const loadMoreContacts = () => {
+    setPageNumber((prevPageNumber) => prevPageNumber + 1);
+  };
+
+  return {
+    contacts,
+    loadMoreContacts,
+  };
 }
 
 export default useContactsAPI;
